cb34d4930fbc3f2110ffac7ed8482fd75815cedd
4L3_xD <alessandra.olea@gmail.com>
2020-05-10 11:52:28 -0300

    add numpy

--- 3neurons.py
+++ 3neurons.py
@@ -15,7 +15,7 @@
 #          ]
 # print(output)
  
-inputs = [1, 2, 3, 2.5]
+""" inputs = [1, 2, 3, 2.5]
 weights = [
             [0.2, 0.8, -0.5, 1.0],
             [0.5, -0.91, 0.26, -0.5],
@@ -31,4 +31,24 @@ for neuron_weights, neuron_bias in zip(weights, biases):
     neuron_output += neuron_bias
     layer_outputs.append(neuron_output)
 
-print(layer_outputs)
+print(layer_outputs) """
+
+import numpy as np
+
+# inputs = [1, 2, 3, 2.5]
+# weights = [0.2, 0.8, -0.5, 1.0]
+# bias = 2
+
+# output = np.dot(weights, inputs) + bias
+# print(output)
+
+inputs = [1, 2, 3, 2.5]
+weights = [
+            [0.2, 0.8, -0.5, 1.0],
+            [0.5, -0.91, 0.26, -0.5],
+            [-0.26, -0.27, 0.17, 0.87]
+          ]
+biases = [2, 3, 0.5]
+
+output = np.dot(weights, inputs) + biases
+print(output)
